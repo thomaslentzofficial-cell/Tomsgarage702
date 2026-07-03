8e0824b072326985cb1d1d3247e6e19a100deb3f
thomaslentzofficial-cell <thomaslentzofficial@gmail.com>
2026-07-02 22:47:49 -0700

    Fix Supabase config by adding quotes around values
    Updated Supabase URL and anon key format to use quotes.

--- supabase-config.js
+++ supabase-config.js
@@ -2,5 +2,5 @@
 // Paste your Supabase Project URL and anon public key below.
 // DO NOT use the service_role key.
 
-window.TG_SUPABASE_URL = https://uldcxvditvcsinwyjsac.supabase.co;
-window.TG_SUPABASE_ANON_KEY = eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVsZGN4dmRpdHZjc2lud3lqc2FjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODMwNDUyNTksImV4cCI6MjA5ODYyMTI1OX0;
+window.TG_SUPABASE_URL = "https://uldcxvditvcsinwyjsac.supabase.co";
+window.TG_SUPABASE_ANON_KEY = "eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVsZGN4dmRpdHZjc2lud3lqc2FjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODMwNDUyNTksImV4cCI6MjA5ODYyMTI1OX0";
